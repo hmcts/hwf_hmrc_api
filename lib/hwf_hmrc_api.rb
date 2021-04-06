f84e40eb86ee2e0579afdca409d234951cbaeae4
Petr Zaparka <petr@zaparka.cz>
2021-04-06 14:02:36 +0100

    Initial commit

---
+++ lib/hwf_hmrc_api.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hwf_hmrc_api/version"
+
+module HwfHmrcApi
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hwf_hmrc_api/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module HwfHmrcApi
+  VERSION = "0.1.0"
+end
