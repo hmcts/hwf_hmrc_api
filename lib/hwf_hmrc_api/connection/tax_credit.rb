5dc5cf25a9a179856646b2ca095064c83a6a1ac5
Petr Zaparka <petr@zaparka.cz>
2021-05-12 14:35:19 +0100

    Extrating date relevant method to helper

--- lib/hwf_hmrc_api/connection/tax_credit.rb
+++ lib/hwf_hmrc_api/connection/tax_credit.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
+require "hwf_hmrc_api/connection/concerns/date_helper"
+
 module HwfHmrcApi
   module TaxCredit
+    include DateHelper
+
     # From Date format: YYYY-MM-DD
     # To Date format: YYYY-MM-DD
     def child_tax_credits(from_date, to_date)
@@ -24,44 +28,6 @@ module HwfHmrcApi
 
     def validate_match_id
       raise HwfHmrcApiError.new("Params validation: Mathching ID is missing", :standard_error) if matching_id.nil?
-    end
-
-    def validate_dates(from_date, to_date)
-      raise_hwf_api_error("FromDate is not a String") unless date_type_valid?(from_date)
-      raise_hwf_api_error("ToDate is not a String") unless date_type_valid?(to_date)
-      raise_hwf_api_error("FromDate is missing") unless date_present?(from_date)
-      raise_hwf_api_error("ToDate is missing") unless date_present?(to_date)
-      raise_hwf_api_error("FromDate format is invalid") unless date_format_valid?(from_date)
-      raise_hwf_api_error("ToDate format is invalid") unless date_format_valid?(to_date)
-    end
-
-    def date_type_valid?(value)
-      return true if value.nil?
-
-      value.is_a?(String)
-    end
-
-    def date_present?(value)
-      return false if value.nil? || value.empty?
-
-      true
-    end
-
-    def date_format_valid?(value)
-      return true if value.match(/\d{4}-\d{2}-\d{2}/) && Date.strptime(value, "%Y-%m-%d")
-
-      false
-    rescue ArgumentError
-      raise HwfHmrcApiError, "Attributes validation: Date format #{value} is invalid"
-    end
-
-    def date_out_of_range?(string)
-      hundred_years_ago = Date.today.year - 100
-      Date.parse(string) <= Date.parse("#{hundred_years_ago}-01-01")
-    end
-
-    def raise_hwf_api_error(message, prefix = "Attributes validation:")
-      raise HwfHmrcApiError, "#{prefix} #{message}"
     end
 
     def request_params(from, to)
